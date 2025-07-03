5302505aa45342bf02be9d913db9fb19880a43ae
Saad Safi <saadsaffi27@gmail.com>
2025-07-03 18:39:40 +0500

    feat: Day 3 - App Router & Server Components, Assignment 1 - Basic Quote Generator display

---
+++ Assignment-1/quote-generator/data/quotes.ts
@@ -0,0 +1,9 @@
+export const motivationalQuotes = [
+  { quote: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+  { quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
+  { quote: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
+  { quote: "Strive not to be a success, but rather to be of value.", author: "Albert Einstein" },
+  { quote: "The mind is everything. What you think you become.", author: "Buddha" },
+  { quote: "The best way to predict the future is to create it.", author: "Peter Drucker" },
+  { quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.", author: "Winston Churchill" }
+];
